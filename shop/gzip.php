a5ef715cff88c1aae0437a9cadc235d1608a1201
hpzeller <hpzeller@xos-shop.com>
2015-05-16 12:09:05 +0200

    update gzip.php

--- shop/gzip.php
+++ shop/gzip.php
@@ -1,47 +1,46 @@
 <?php
-//turn off all error reporting
+// turn off all error reporting
 error_reporting(0);
   
 $allowed = array('css','js','svg','ttf'); //set array of allowed file types to prevent abuse
   
-//check for request variable existence and that file type is allowed
-if(isset($_GET['file']) && isset($_GET['type']) && in_array(substr($_GET['file'],strrpos($_GET['file'],'.')+1), $allowed)){
+// check for request variable existence and that file type is allowed
+if (isset($_GET['file']) && isset($_GET['type']) && in_array(substr($_GET['file'],strrpos($_GET['file'],'.')+1), $allowed)){
     $data = file_get_contents(dirname(__FILE__).'/'.$_GET['file']); // grab the file contents
-  
-    $etag = '"'.md5($data).'"'; // generate a file Etag
-    header('Etag: '.$etag); // output the Etag in the header
-  
+    $last_modified = filemtime(dirname(__FILE__).'/'.$_GET['file']); // get the last-modified-date of the file  
+    $etag = '"' . md5($last_modified . $data) . '"'; // generate a file Etag 
+    
+    header('Etag: ' . $etag); // output the Etag in the header
+    header('Vary: Accept-Encoding'); // make sure proxy's store both a compressed and uncompressed version of the file 
+    
+    // check if file has changed. If not, send 304 and exit  
+    if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $etag == trim($_SERVER['HTTP_IF_NONE_MATCH'])) {  
+        header('HTTP/1.1 304 Not Modified');
+        exit;
+    }    
+    
     // output the content-type header for each file type
     switch ($_GET['type']) {
         case 'css':       
-            header ("Content-Type: text/css; charset=UTF-8");
+            header('Content-Type: text/css');
         break;
   
         case 'js':       
-            header ("Content-Type: text/javascript; charset=UTF-8");
+            header('Content-Type: text/javascript');
         break;
 
         case 'svg':
-            header ("Content-Type: image/svg+xml");
+            header('Content-Type: image/svg+xml');
         break;
 
         case 'ttf':        
-            header ("Content-Type: application/x-font-ttf");
+            header('Content-Type: application/x-font-ttf');
         break;
     }
-  
-    header('Cache-Control: max-age=604800, must-revalidate'); //output the cache-control header
-    $offset = 60 * 60 * 24 * 7;
-    $expires = 'Expires: ' . gmdate('D, d M Y H:i:s',time() + $offset) . ' GMT'; // set the expires header to be 1 hour in the future
-    header($expires); // output the expires header
-  
-    // check the Etag the browser already has for the file and only serve the file if it is different
-    if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $etag == stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) {
-        header('HTTP/1.1 304 Not Modified');
-        header('Content-Length: 0');
-    } else {
-        header ("Content-Encoding: gzip");
-        echo gzencode($data, 9);
-    }
+
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $last_modified) . ' GMT'); // set last-modified header   
+    header('Content-Encoding: gzip'); 
+    
+    echo gzencode($data, 9);
 }
 ?>
