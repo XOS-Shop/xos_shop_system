49fd1be01698e2431c7c58eab68c0806aa8fe70e
hpzeller <hpzeller@xos-shop.com>
2018-03-01 01:47:17 +0100

    update outputfilter.set_internal_link.php

--- shop/smarty/Smarty-3.1.31/plugins/outputfilter.set_internal_link.php
+++ shop/smarty/Smarty-3.1.31/plugins/outputfilter.set_internal_link.php
@@ -12,5 +12,15 @@ function smarty_outputfilter_set_internal_link($source, Smarty_Internal_Template
 {
     return preg_replace_callback('#\[@\{link(.*)\}@\]#siU', function($match) {return eval('return '.$match[1].';');}, $source);
 }
+/*
+function smarty_outputfilter_set_internal_link($source, Smarty_Internal_Template $smarty)
+{
+    return preg_replace_callback('#\[@\{link(.*)\}@\]#siU', 'set_internal_link', $source);
+}
 
+function set_internal_link($match) 
+{   
+    return eval('return '.$match[1].';');
+}
+*/
 ?>
